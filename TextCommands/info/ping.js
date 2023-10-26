3a23b265611825ddd9ff3ffbac17bd3a1bc8bd7f
KrySoSad <duythuc070205@gmail.com>
2023-10-26 22:04:38 +0700

    Nothing here :)

--- TextCommands/info/ping.js
+++ TextCommands/info/ping.js
@@ -1,16 +1,17 @@
 const { Client, Message } = require("discord.js");
-
+const ownerId = require('../../config.json').OWNER_ID;
 module.exports = {
-    name: "ping",
-    aliases: ["p"],
-    description: "Xem độ trễ của bot",
-    /**
-     * 
+  name: "ping",
+  aliases: ["p"],
+  cooldowns: 3,
+  description: "Xem độ trễ của bot",
+  /**
+   * 
      * @param {Client} client 
      * @param {Message} message 
      * @param {*} args 
-     */
-    run: async (client, message, args) => {
+  */
+ run: async (client, message, args) => {
       const dt = new Date() - new Date(message.createdTimestamp);
       await message.reply(`**Độ trễ tin nhắn** \`${dt}ms\`\n**Websocket** : \`${client.ws.ping}ms\``)
   }
