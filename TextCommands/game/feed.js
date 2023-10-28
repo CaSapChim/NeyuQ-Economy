faaf783a3ac08af18f2c7d403538b1d644d7c80c
KrySoSad <duythuc070205@gmail.com>
2023-10-28 21:54:47 +0700

    40%

--- TextCommands/game/feed.js
+++ TextCommands/game/feed.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const feedAnimalModel = require('../../database/models/userDataJob/feedAnimalModel');
+const emoji = require('../../emoji.json');
 
 module.exports = {
     name: 'feed',
@@ -11,50 +12,19 @@ module.exports = {
      * @param {*} args 
      */
     run: async(client, message, args) => {
-        const animalArr = ['bo', 'ga'];
-        const author = message.author.id;
-        const nameAnimal = args[0];
-        let soLuong = parseInt(args[1]);
+        const idAnimal = args[0];
+        let amount = args[1];
+        const author = message.author;
+        if (amount < 0 || (isNan(amount) && amount != "all"))
+            return message.reply(`${emoji.fail} Phắc du <@${author.id}> sai định dạng rồi`)
+            .then(msg => {
+                setTimeout(() => {
+                    msg.delete();
+                }, 3000);
+            });
         
-        const animalEmoji = {
-            'con gà': '<:Chicken17:1156557573219168307>',
-            'con bò': '<:3331_minecraft_cow:1156555169396428830>'
-        }
-        
-        let soLuongAnimal = await client.xemAnimal(author, checkAnimal(nameAnimal)); // check số animal chưa cho ăn trong db
-        let dataFed = await feedAnimalModel.findOne({ userId: author, name: checkAnimal(nameAnimal) });
-        if (!dataFed) {
-            dataFed = new feedAnimalModel({
-                userId: author, 
-                name: checkAnimal(nameAnimal)
-            })
-        }
-        if (!nameAnimal || !animalArr.includes(nameAnimal)) return message.reply('Sai cú pháp.\nCách dùng: `nqg feed <bo/ga> <số lượng>`');
-        if (soLuongAnimal <= 0) return message.reply(`Bạn không có **${checkAnimal(nameAnimal)}** nào trong chuồng`)
-        if (!soLuong || soLuong < 0) soLuong = 1;
-        if (soLuong > soLuongAnimal) soLuong = soLuongAnimal;
-        if (soLuongAnimal == 0) return message.reply(`Bạn đã cho ${checkAnimal(nameAnimal)} ăn đầy đủ`);
-
-        let foodEmoji = {
-            "lúa" : "cây lúa <:LC_Wheat:1155701062670504037>",
-            "hạt lúa" : "hạt lúa <:seeds97:1155701097806180372>",
-        };
-		let soLuongLua = await client.nongSan(author, "lúa");
-		let soLuongHatLua = await client.nongSan(author, "hạt lúa");
-		if (nameAnimal == 'bo' && soLuongLua < soLuong) return message.reply('Bạn không đủ lúa cho bò ăn <:LC_Wheat:1155701062670504037>');
-		if (nameAnimal == 'ga' && soLuongHatLua < soLuong) return message.reply('Bạn không đủ hạt lúa cho gà ăn <:seeds97:1155701097806180372>');
-        await client.choAn(author, checkAnimal(nameAnimal), soLuong);
-        await client.truNongSan(author, checkFood(nameAnimal), soLuong);
-        await message.reply(`Bạn đã cho **${soLuong} ${checkAnimal(nameAnimal)} ${animalEmoji[checkAnimal(nameAnimal)]}** ăn thành công **${foodEmoji[checkFood(nameAnimal)]}**`);
-
-        function checkAnimal(animal) {
-            if (animal == 'bo') return animal = 'con bò';
-            if (animal == 'ga') return animal = 'con gà';
-        }
-
-        function checkFood(animal) {
-            if (animal == 'bo') return animal = "lúa";
-            if (animal == 'ga') return animal = "hạt lúa";
+        const idAnimalToAnimal = {
+            
         }
     }
 }
