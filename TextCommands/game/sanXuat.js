a0aa5bc26479811fcdbecbf48b9619e824a69d63
KrySoSad <duythuc070205@gmail.com>
2023-11-16 22:58:10 +0700

    80% - Làm tới phần thêm nl và tru nl

--- TextCommands/game/sanXuat.js
+++ TextCommands/game/sanXuat.js
@@ -1,4 +1,8 @@
 const Discord = require('discord.js');
+const data = require('../../data/nguyenLieu.json');
+const emoji = require('../../emoji.json');
+
+const am = data.sanPham;
 
 module.exports = {
     name: "sanxuat",
@@ -16,16 +20,20 @@ module.exports = {
             .setColor("Green")
             .setTitle("BẢNG NGUYÊN LIỆU")
             .setDescription(`
-                wudgawydwad  
+                test test test test  
             `)
             .addFields(
                 {name: "Thức ăn cho động vật", value: `
-                16 <:LC_Wheat:1155701062670504037> => 3 <:04c48f45474c4130a2fb5ff3a3939268:1166927884187877417>
-                8 <:daunh_1:1156608655060381760> + 8 <:seeds97:1155701097806180372> => 3 <:chickenFeed:1166927888336027748>
-                8 <:Carrot29:1166650013603090432> + 8 <:potato45:1166650017264705547> => 3 <:pigFeed:1166929189681766461>
+                **\`300\`** | ${am.thucAnBo.lua} ${emoji.lua} => 1 ${emoji.thucAnBo}
+                **\`301\`** | ${am.thucAnGa.hatDau} ${emoji.hatDau} + ${am.thucAnGa.hatLua} ${emoji.hatLua} => 1 ${emoji.thucAnGa}
+                **\`302\`** | ${am.thucAnHeo.caRot} ${emoji.caRot} + ${am.thucAnHeo.khoaiTay} ${emoji.khoaiTay} => 1 ${emoji.thucAnHeo}
                 `,inline: false},
                 {name: `Sản phẩm khác`, value: `
-                8 <:LC_Wheat:1155701062670504037> => 2 <:6289_flour:1155701022891704360>
+                **\`306\`** | ${am.botMi.lua} ${emoji.lua} => 1 ${emoji.botMi}
+                **\`307\`** | ${am.butter.sua} ${emoji.sua} + ${am.butter.trung} ${emoji.trung} => 1 ${emoji.butter}
+                **\`308\`** | ${am.banhMi.botMi} ${emoji.botMi} => 1 ${emoji.banhMi} 
+                **\`309\`** | ${am.shushi.lua} ${emoji.lua} + ${am.shushi.veryCommon} ${emoji.veryCommon} => 1 ${emoji.shushi} 
+                **\`310\`** | ${am.caDongHop.common} ${emoji.common} => 1 ${emoji.caDongHop}
                 `, inline: true}
             )
             .setTimestamp()
@@ -34,13 +42,13 @@ module.exports = {
             new Discord.ButtonBuilder()
                 .setCustomId('sanxuat')
                 .setStyle(Discord.ButtonStyle.Primary)
-                .setLabel("Sản xuất vật phẩm"),
+                .setLabel("Sản xuất vật phẩm")
         )
         const a = await message.reply({ embeds: [embed], components: [buttonRow] });
         var collector = a.createMessageComponentCollector({
             filter: (interaction) =>
               (interaction.isButton() || interaction.isStringSelectMenu() || interaction.isModalSubmit()) &&
-              interaction.message.author.id == client.user.id, time: 60000
+              interaction.message.author.id == client.user.id, time: 30000
         });
 
         collector.on("collect", async (interaction) => {
@@ -52,7 +60,7 @@ module.exports = {
                 const idVatPham = new Discord.TextInputBuilder()
                     .setCustomId('idVatPham')
                     .setLabel("Nhập id của vật phẩm muốn sản xuất")
-                    .setPlaceholder("Vd: 153")
+                    .setPlaceholder("Vd: 307")
                     .setRequired(true)
                     .setStyle(Discord.TextInputStyle.Short)
                 const soLuongVatPham = new Discord.TextInputBuilder()
@@ -67,11 +75,108 @@ module.exports = {
                 await interaction.showModal(modal);
             }
             const filter = (interaction) => interaction.customId == "sanxuatmodal";
-            interaction.awaitModalSubmit({ filter, time: 60_000})
-            .then(interaction => {
-                const t1 = interaction.fields.getTextInputValue("idVatPham");
-                const t2 = interaction.fields.getTextInputValue("soLuongVatPham");
-            })
+            interaction.awaitModalSubmit({ filter, time: 30_000})
+            .then(async interaction => {
+                let t1 = interaction.fields.getTextInputValue("idVatPham");
+                let t2 = interaction.fields.getTextInputValue("soLuongVatPham");
+
+                const idSphamPhamObj = {
+                    "300": "thucAnBo",
+                    "301": "thucAnGa",
+                    "302": "thucAnHeo",
+                    "306": "botMi",
+                    "307": "butter",
+                    "308": "banhMi",
+                    "309": "shushi",
+                    "310": "caDongHop",
+                }
+
+                const nameSP = {
+                    "300": "thức ăn bò",
+                    "301": "thức ăn gà",
+                    "302": "thức ăn heo",
+                    "306": "bột mì",
+                    "307": "bơ",
+                    "308": "bánh mì",
+                    "309": "shushi",
+                    "310": "cá đóng hộp",
+                }
+
+                const nameNL = {
+                    "lua": "lúa",
+                    "hatLua": "hạt lúa",
+                    "hatDau": "hạt đậu",
+                    "caRot": "cà rốt",
+                    "khoaiTay": "khoai tây",
+                    "bi": "bí",
+                    "trung": "trứng",
+                    "sua": "sữa",
+                    "botMi": "bột mì",
+                    "common": "Common",
+                    "veryCommon": "Very Common",
+                    "butter": "bơ",
+                    "banhMi": "bánh mì"
+                }
+
+                const property = idSphamPhamObj[t1];
+                const dataNguyenLieu = data.sanPham;
+
+                if (typeof idSphamPhamObj[t1] === "undefined") 
+                    return interaction.reply({ content: "Bạn không nhập đúng id của món đồ muốn sản xuất", ephemeral: true});
+
+                if (parseInt(t2) < 0) 
+                    return interaction.reply({ content: "Bạn không được nhập số âm", ephemeral: true });
+
+                    const obj = {
+                        "veryCommon": await client.ca(message.author.id, "Very Common"),
+                        "common": await client.ca(message.author.id, "Common"),
+                        "hatLua": await client.nongSan(message.author.id, "hạt lúa"),
+                        "hatDau": await client.nongSan(message.author.id, "hạt đậu"),
+                        "hatDuaHau": await client.nongSan(message.author.id, "hạt dưa hấu"),
+                        "khoaiTay": await client.nongSan(message.author.id, "khoai tây"),
+                        "caRot": await client.nongSan(message.author.id, "cà rốt"),
+                        "lua": await client.nongSan(message.author.id, "lúa"),
+                        "bi": await client.nongSan(message.author.id, "bí"),
+                        "duaHau": await client.nongSan(message.author.id, "dưa hấu"),
+                        "sua": await client.sanPham(message.author.id, "sữa"),
+                        "trung": await client.sanPham(message.author.id, "trứng"),
+                        "thitHeo": await client.sanPham(message.author.id, "thịt heo"),
+                        "botMi": await client.sanPham(message.author.id, "bột mì"),
+                        "bo": await client.sanPham(message.author.id, "bơ"), 
+                        "banhMi": await client.sanPham(message.author.id, "bánh mì"), 
+                    }
+
+                for (const key in dataNguyenLieu[property]) {
+                    
+                    if (obj[key] < 0 || obj[key] < parseInt(t2) * dataNguyenLieu[property][key]) {
+                        return interaction.reply({ content: "Bạn không đủ nguyên liệu để là sản phẩm này!", ephemeral: true });
+                    }
+                    await client.truSanPham(interaction.user.id, nameNL[key], parseInt(t2) * dataNguyenLieu[property][key]);
+                    await client.truNongSan(interaction.user.id, nameNL[key], parseInt(t2) * dataNguyenLieu[property][key]);
+                    await client.truCa(interaction.user.id, nameNL[key], parseInt(t2) * dataNguyenLieu[property][key]);
+                    console.log(obj[key]);
+                    console.log(`${key} - ${dataNguyenLieu[property][key]}`);
+                }
+                
+                await client.addSanPham(interaction.user.id, nameSP[t1], parseInt(t2));
+                console.log(`Đã sản xuất thành công ${t2} ${nameSP[t1]}`);
+
+                let successEmbed = new Discord.EmbedBuilder()
+                    .setDescription(`Bạn vừa sản xuất thành công ${t2} ${nameSP[t1]} ${emoji[property]}`)
+                    .setTimestamp()
+                    .setColor("Green")
+
+                await interaction.channel.send({ content: `<@${interaction.user.id}>`, embeds: [successEmbed] });
+
+            }).catch(err => {
+                console.log(err);
+            });
         });
+
+        collector.on("end", () => {
+            buttonRow.components[0].setDisabled(true);
+            embed.setColor("Grey");
+            a.edit({ content: `Tin nhắn hết hiệu lực`, components: [buttonRow], embeds: [embed] });
+        })
     }
 }
